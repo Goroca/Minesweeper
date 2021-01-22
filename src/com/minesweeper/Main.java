3b7bc8ac175975dd7530bd7aa6d1b3418fee9c89
Carlos <gorocarlos98@gmail.com>
2021-01-22 13:54:04 +0100

    Define Variables

---
+++ src/com/minesweeper/Main.java
@@ -0,0 +1,11 @@
+package com.minesweeper;
+
+public class Main extends Methods{
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/minesweeper/Mine.java
@@ -0,0 +1,14 @@
+package com.minesweeper;
+
+public class Mine {
+	private boolean active;
+	private int around;
+	private boolean targeted;
+	private boolean hidden;
+
+	
+	public Mine() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
